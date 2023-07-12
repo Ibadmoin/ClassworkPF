ea986691345a38b43386cbd4324fb5b3a5fd1cb5
Ibad Moin <123115847+Ibadmoin@users.noreply.github.com>
2023-07-12 20:50:29 +0500

    Delete PF-RollNo58/lab01-02 directory

--- PF-RollNo58/lab01-02/new1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
- #include<conio.h> 
-
-void main(void)
-{
-    printf("Welcome to the wonderful world of C/C++!!!\n");
-    getch(); // add this, will show the output screen
-};
--- PF-RollNo58/lab01-02/task.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-// take two inputs from user and sawp their variables
-
-int main(){
-    
-
-    return 0;
-}
